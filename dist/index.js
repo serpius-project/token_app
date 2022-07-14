f1d444a8160ae8f1a98e94bb7711e927488ea22c
mmendozaj <94900477+mmendozaj@users.noreply.github.com>
2022-07-14 10:30:23 +0200

    minor changes

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58700" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49875" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
